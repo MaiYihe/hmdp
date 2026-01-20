212670fc154a50500bf045ed08d7deda11b03794
maiyihe <2656377727@qq.com>
2026-01-20 19:52:19 +0800

    add：秒杀下单，生产者部分

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,21 +1,20 @@
 package com.hmdp.service.impl;
 
-import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import javax.annotation.Resource;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.dto.Result;
-import com.hmdp.entity.SeckillVoucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
 import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisIdWorker;
 import com.hmdp.utils.UserHolder;
 
@@ -36,48 +35,43 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
 
     @Resource
     private RedisIdWorker redisIdWorker;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    // Lua return 的结果 → Java 里的 Long
+    @Resource
+    private DefaultRedisScript<Long> seckillScript;
 
     @Override
-    @Transactional
     public Result seckillVoucher(Long voucherId) {
-        // 1. 查询优惠券
-        SeckillVoucher voucher = seckillVoucherService.getById(voucherId);
-        if (voucher == null) {
-            return Result.fail("秒杀券不存在");
-        }
-        // 2. 判断秒杀是否结束
-        if (voucher.getBeginTime().isAfter(LocalDateTime.now())) {
-            return Result.fail("秒杀尚未开始！");
-        }
-        if (voucher.getEndTime().isBefore(LocalDateTime.now())) {
-            return Result.fail("秒杀已经结束");
-        }
-        // 3. 判断库存是否充足
-        if (voucher.getStock() < 1) {
-            return Result.fail("库存不足！");
-        }
-
-        // 3. 扣减库存
-        LambdaUpdateWrapper<SeckillVoucher> uw = Wrappers.lambdaUpdate(SeckillVoucher.class)
-                .eq(SeckillVoucher::getVoucherId, voucherId)
-                .gt(SeckillVoucher::getStock, 0)
-                .setSql("stock = stock - 1");
-        boolean success = seckillVoucherService.update(uw);
-        if (!success) {
-            return Result.fail("库存不足！");
-        }
-
-        // 4. 创建订单
-        VoucherOrder voucherOrder = new VoucherOrder();
-        long orderId = redisIdWorker.nextId("order");
-        voucherOrder.setId(orderId);
-
+        //  查询 userId ，生成 orderId
         Long userId = UserHolder.getUser().getId();
-        voucherOrder.setUserId(userId);
-        voucherOrder.setVoucherId(voucherId);
+        long orderId = redisIdWorker.nextId("order");
 
-        this.save(voucherOrder);
+        // 运行 Redis Lua 原子脚本
+        String stockKey = RedisConstants.SECKILL_STOCK_KEY + voucherId;
+        String orderKey = RedisConstants.SECKILL_ORDER_KEY + voucherId;
+        String streamKey = RedisConstants.SECKILL_STREAM_KEY;
 
+        Long luaResult = stringRedisTemplate.execute(
+                seckillScript,
+                Arrays.asList(stockKey, orderKey, streamKey),
+                userId.toString(),
+                voucherId.toString(),
+                String.valueOf(orderId));
+
+        // 返回错误值
+        if (luaResult == null) {
+            return Result.fail("系统异常");
+        }
+        if (luaResult.intValue() == 1) {
+            return Result.fail("库存不足");
+        }
+        if (luaResult.intValue() == 2) {
+            return Result.fail("重复下单");
+        }
+
+        // Lua 校验通过 —— 返回 orderId（这里不写 DB）
         return Result.ok(orderId);
     }
 
